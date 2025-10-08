02cf04f23b8c48f2cba390b06ef677b24bb3704d
devart901-debug <devart901-debug@users.noreply.github.com>
2025-10-08 16:54:00 +1000

    повторение

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
 from . import telegram_handlers
+from . import views
 
 urlpatterns = [
-    path('webhook/telegram/', telegram_handlers.telegram_webhook, name='telegram_webhook'),
+    path('telegram/', telegram_handlers.telegram_webhook, name='telegram_webhook'),
     path('test/', views.test_page, name='test_page'),
 ]
