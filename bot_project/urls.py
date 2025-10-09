4bd4aacbbc300d4bc26b9422695b5b2c1cd10bb1
devart901-debug <devart901-debug@users.noreply.github.com>
2025-10-09 14:37:39 +1000

    123

--- bot_project/urls.py
+++ bot_project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from campaigns import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/')),  # Редирект на админку
+    path('admin/', admin.site.urls),
+    path('campaigns/', include('campaigns.urls')),
     path('', views.home_page, name='home'),
     path('webhook/', include('campaigns.urls')),
 ]
