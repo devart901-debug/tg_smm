af18ddd01a47e1d2575930c0dbd543d4ed3c6b80
devart901-debug <devart901-debug@users.noreply.github.com>
2025-10-09 15:04:18 +1000

    Упал сервер

--- campaigns/apps.py
+++ campaigns/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CampaignsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'campaigns',
-
+    verbose_name = 'Мероприятия'
